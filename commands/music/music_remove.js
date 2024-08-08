006445322e8123c635b3e1f6a75799b7c59a8f37
rickwengdev <rick783256@gmail.com>
2024-08-08 09:15:15 +0800

    add agent

--- commands/music/music_remove.js
+++ commands/music/music_remove.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js'
-import { getMusicPlayer } from '../../datapackage/musicfunction/playerManager.js'
+import { getMusicPlayer, agent } from '../../datapackage/musicfunction/playerManager.js'
 
 // 定義 slash command 的結構
 export const data = new SlashCommandBuilder()
